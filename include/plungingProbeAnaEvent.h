8a0c5a848dc5af5cab937761faf0ad097f2f12e5
David Flay <flay@umass.edu>
2018-09-29 10:08:20 -0500

    Increased the possible number of events in the PP data struct

--- include/plungingProbeAnaEvent.h
+++ include/plungingProbeAnaEvent.h
@@ -2,22 +2,23 @@
 #define PLUNGING_PROBE_ANA_EVENT_H
 
 // a plunging probe event where we group the N NMR-DAQ events 
-// into a single "analysis" event. We'll never have more than 25 shots 
-// per NMR-DAQ run, so we'll cap the arrays at 25 
+// into a single "analysis" event.  
+
+#define PP_MAX_EVENTS 5000
 
 typedef struct plungingProbeAnaEvent{
-   unsigned long long time[25];     // average time of the event  
-   double r[25];                    // radial coordinate
-   double y[25];                    // vertical coordinate 
-   double phi[25];                  // azimuthal coordinate 
-   double temp[25];                 // average temperature (deg C) 
-   double temp_err[25];             // temperature uncertainty (deg C) 
-   double freq[25];                 // average frequency (Hz) 
-   double freq_err[25];             // frequency uncertainty (Hz)
-   double freq_LO[25];              // local oscillator (Hz) 
-   double freq_RF[25];              // pi/2 frequency (Hz)
-   int run;                         // NMR-DAQ run   
-   int numTraces;                   // number of traces for the run 
+   unsigned long long time[PP_MAX_EVENTS];     // average time of the event  
+   double r[PP_MAX_EVENTS];                    // radial coordinate
+   double y[PP_MAX_EVENTS];                    // vertical coordinate 
+   double phi[PP_MAX_EVENTS];                  // azimuthal coordinate 
+   double temp[PP_MAX_EVENTS];                 // average temperature (deg C) 
+   double temp_err[PP_MAX_EVENTS];             // temperature uncertainty (deg C) 
+   double freq[PP_MAX_EVENTS];                 // average frequency (Hz) 
+   double freq_err[PP_MAX_EVENTS];             // frequency uncertainty (Hz)
+   double freq_LO[PP_MAX_EVENTS];              // local oscillator (Hz) 
+   double freq_RF[PP_MAX_EVENTS];              // pi/2 frequency (Hz)
+   int run;                                    // NMR-DAQ run   
+   int numTraces;                              // number of traces for the run 
 } plungingProbeAnaEvent_t;  
 
 #endif  
